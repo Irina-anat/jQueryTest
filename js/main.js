50256d325da582b09b68f057abeabdcf2ef8d458
Iryna Rozmaita <iriszechka@ukr.net>
2023-12-21 17:17:51 +0200

    add gallery

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $('.small a').click(function (e) {
+        if ($('.big img').attr('src') !== $(this).attr('href')) {
+            $('.big img').hide().attr('src', $(this).attr('href')).fadeIn(1000)
+        }
+        e.preventDefault()
+    });
+    $('.button').click(function () {
+        $('.gallery').slideToggle(500);
+        if ($('.button').text() == 'hide') {
+            $('.button').text('show')
+        } else {
+            $('.button').text('hide')
+        }
+    });
+    $('.small a img').click(function () {
+        $('.small a img').fadeTo(500, 1).css({
+         'border':'none'
+     })  
+        $(this).fadeTo(500, 0.6).css({
+        'border':'2px solid green'
+    })
+})
+
+});
